2b05acfd4a5773c3c6054d8e5430b6c550971c07
Andreas Alexander Maier <andb0t@pm.me>
2018-11-14 12:49:51 +0100

    Add flake8 to hook

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+EXIT_CODE=0
+
+
+
 # Fail if trying to commit executed jupyter notebook
  diff=$(git diff --cached | grep -e '^[\+]\s.*' | grep -e '\"execution_count\": [0-9][0-9]*')
 
@@ -8,5 +12,19 @@
    # echo "[POLICY] Jupyter notebook test passed"
  else
    echo "[POLICY] Error: Please clear the output of the jupyter notebook before committing"
-   exit 1
+   EXIT_CODE=1
  fi
+
+
+
+ # flake8
+CHANGED_PYTHON_FILES=$(git diff --cached --name-status | grep '.py' | awk '{print $2}' |  tr '\n' ' ')
+# shellcheck disable=SC2086
+FLAKE_ERRORS=$(flake8 $CHANGED_PYTHON_FILES | tee)
+if  [[ $FLAKE_ERRORS ]]; then
+  echo "[POLICY] Error: Please make code flake8 conform before committing"
+  echo "$FLAKE_ERRORS"
+  EXIT_CODE=1
+fi
+
+exit $EXIT_CODE
