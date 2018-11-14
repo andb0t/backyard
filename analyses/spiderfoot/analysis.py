7d776afc8c1a3cee8cfa08ff0b1320b1f0f37713
Yuan <yuan.gao@ovgu.de>
2018-11-14 12:32:06 +0100

    Fix remaining code conventions reported by pylint

--- analyses/spiderfoot/analysis.py
+++ analyses/spiderfoot/analysis.py
@@ -6,42 +6,44 @@ import pandas as pd
 import spacy
 
 
-nlpEN = spacy.load('en')
-nlpDE = spacy.load('de')
+NLP_EN = spacy.load('en')
+NLP_DE = spacy.load('de')
 
 
-def getModuleDummy(name):
+def get_module_dummy(name):
+    """Return one module object contains nothing."""
     module = {}
     module['Module'] = name
     return module
 
 
-def isGermanName(name):
+def is_german_name(name):
+    """Try to filter german words which are not human names."""
     # name-like entries, e.g. "Kostenlose Service-Hotline" should return false
-    humanName = False
+    is_human_name = False
 
-    doc = nlpDE(name)
-    endIndex = 0
+    doc = NLP_DE(name)
+    end_index = 0
     for ent in doc.ents:
         # print(ent.text, ent.start_char, ent.end_char, ent.label_)
-        humanName = True
-        endIndex = ent.end_char
+        is_human_name = True
+        end_index = ent.end_char
         if ent.label_ != "PER":
-            humanName = False
+            is_human_name = False
             break
 
-    if humanName and endIndex < len(name):
+    if is_human_name and end_index < len(name):
         # including unprocessed part
-        humanName = False
+        is_human_name = False
 
-    return humanName
+    return is_human_name
 
 #
 # def isEnglishName(name):
 #     #name-like entries, e.g. "Kostenlose Service-Hotline" should return false
 #     humanName = False
 #
-#     doc = nlpEN(name)
+#     doc = NLP_EN(name)
 #     endIndex = 0
 #     for ent in doc.ents:
 #         #print(ent.text, ent.start_char, ent.end_char, ent.label_)
@@ -57,11 +59,12 @@ def isGermanName(name):
 #     return humanName
 
 
-def filterNames(name_data):
+def filter_names(name_data):
+    """Try to filter words which are not human names."""
     removal_list = []
-    for name, count in name_data.items():
+    for name in name_data.items():
         # name-like entries, e.g. "Kostenlose Service-Hotline" could be removed here
-        if not isGermanName(name):
+        if not is_german_name(name):
             removal_list.append(name)
 
     print("Count of names:", len(name_data))
@@ -73,13 +76,17 @@ def filterNames(name_data):
     return name_data
 
 
-def getCountsData(df, module_name, data_type):
-    series = df.loc[(df['Module'] == module_name) & (df['Type'] == data_type)]['Data'].value_counts()
+def get_counts_data(data_frame, module_name, data_type):
+    """Get the data column with their counts as a panda series."""
+    typed_data = data_frame.loc[(data_frame['Module'] == module_name) &
+                                (data_frame['Type'] == data_type)]
+    series = typed_data['Data'].value_counts()
     return series
 
 
-def getMapData(df, module_name, data_type, reverse=False):
-    data = df.loc[(df['Module'] == module_name) & (df['Type'] == data_type)]
+def get_mapped_data(data_frame, module_name, data_type, reverse=False):
+    """Get the mapping data from the Source column and the Data column as a panda series."""
+    data = data_frame.loc[(data_frame['Module'] == module_name) & (data_frame['Type'] == data_type)]
 
     series = None
     if not reverse:
@@ -91,25 +98,26 @@ def getMapData(df, module_name, data_type, reverse=False):
 
 
 # the major method defines which data to abstract and how to abstract them
-def getData(df, module_name, data_type):
+def get_data(data_frame, module_name, data_type):
+    """Get the data from a specified module."""
     data = None
 
     # sfp_names
     if module_name == 'sfp_names':
         # only HUMAN_NAME is handled while no other type exists in current data file
         if data_type == 'HUMAN_NAME':
-            data = getCountsData(df, module_name, data_type)
+            data = get_counts_data(data_frame, module_name, data_type)
             # name-like entries, e.g. "Kostenlose Service-Hotline" could be removed here
-            data = filterNames(data)
+            data = filter_names(data)
 
     # sfp_dnsresolve
     if module_name == 'sfp_dnsresolve':
         if data_type == 'IP_ADDRESS':
-            data = getMapData(df, module_name, data_type)
+            data = get_mapped_data(data_frame, module_name, data_type)
 
         if data_type == 'AFFILIATE_INTERNET_NAME':
-            # TODO: value from this type looks like a IP-Host mapping but why it is affilicated?
-            data = getMapData(df, module_name, data_type, True)
+            # Notice: value from this type looks like a IP-Host mapping but why it is affilicated?
+            data = get_mapped_data(data_frame, module_name, data_type, True)
 
     if data is not None:
         # dict is automatically serializable
@@ -118,23 +126,24 @@ def getData(df, module_name, data_type):
     return data
 
 
-def getTypes(df, module_name):
-    return df.loc[df['Module'] == module_name]['Type'].unique()
+def get_types(data_frame, module_name):
+    """Get data types belong to one module."""
+    return data_frame.loc[data_frame['Module'] == module_name]['Type'].unique()
 
 
-def getModule(df, module_name):
-    # get module data if defined
+def get_module(data_frame, module_name):
+    """Get module data if defined."""
     result = {}
-    for data_type in getTypes(df, module_name):
-        data = getData(df, module_name, data_type)
+    for data_type in get_types(data_frame, module_name):
+        data = get_data(data_frame, module_name, data_type)
         if data is not None:
             result[data_type] = data
 
-    if len(result) == 0:
+    if not result:
         # no data exists or defined
         return None
 
-    module = getModuleDummy(module_name)
+    module = get_module_dummy(module_name)
     module['Result'] = result
 
     return module
@@ -144,17 +153,17 @@ def run(data_dir):
     """Call the analysis."""
     print('Opening datafile in ' + data_dir)
 
-    with open(data_dir + '/paths.json') as f:
-        json_data = json.load(f)
+    with open(data_dir + '/paths.json') as file:
+        json_data = json.load(file)
 
     csv_file = data_dir + '/' + json_data['data']['file']
-    df = pd.read_csv(csv_file, parse_dates=['Last Seen'], engine='python')
+    data_frame = pd.read_csv(csv_file, parse_dates=['Last Seen'], engine='python')
 
     # get data by SpiderFoot modules
-    resultName = getModule(df, "sfp_names")
-    resultDNS = getModule(df, "sfp_dnsresolve")
+    result_name = get_module(data_frame, "sfp_names")
+    result_dns = get_module(data_frame, "sfp_dnsresolve")
 
-    result = json.dumps([resultName, resultDNS], indent=4)
+    result = json.dumps([result_name, result_dns], indent=4)
 
     print(json.dumps(result, indent=4))
 
