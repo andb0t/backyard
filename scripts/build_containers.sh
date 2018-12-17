95c49068a872787464b57214c25114ba9eb93b1a
Andreas Alexander Maier <andb0t@pm.me>
2018-12-17 16:38:25 +0100

    Add dummy output stub

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -8,3 +8,4 @@ docker build -t backyard/analyzer-example:latest templates/analyzer/example
 docker build -t backyard/analyzer-dummy:latest modules/analyzer/dummy
 docker build -t backyard/scanner-theharvester:latest modules/scanner/theharvester
 docker build -t backyard/analyzer-data_statistics:latest modules/analyzer/data_statistics
+docker build -t backyard/analyzer-dummy:latest modules/analyzer/dummy
