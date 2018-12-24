26a2b51a074536a524ab251df811b11066a842fa
Andreas Alexander Maier <andb0t@pm.me>
2018-12-24 15:40:52 +0100

    Separate scanner from architecture logic

--- modules/scanner/theharvester/src/backyard/module/theharvester/__main__.py
+++ modules/scanner/theharvester/src/backyard/module/theharvester/__main__.py
@@ -1,14 +1,63 @@
 import asyncio
 import json
 import os
-import time
-import random
 from xml.etree import ElementTree
 
 from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrNoServers
 
 import backyard.api.proto.api_pb2 as api
+
+
+def execute(domain, scanner_id):
+    # define output paths
+    folder = '/data/' + domain
+    file_base = folder + '/' + scanner_id
+    xml_file = file_base + '.xml'
+    json_file = file_base + '.json'
+    print('Saving to file {}'.format(xml_file))
+
+    # define command
+    data_source = "bing"
+    # theharvester fails at splitting differentiating dir names including a
+    # dot from file extensions. So we have to redefine the file name, enter
+    # the data directory first and then execute
+    html_file = os.path.basename(xml_file) + '.html'
+    _cmd = "cd {} && theharvester -d {} -b {} -f {}".format(folder, domain, data_source, html_file)
+
+    # run it
+    print("Executing: " + _cmd)
+    os.system(_cmd)
+
+    return json_file, xml_file
+
+
+def parse(xml_file):
+    search_strings = {'email': 'email',
+                      'host_name': 'host',
+                      'virtual_host_name': 'vhost',
+                      'tld': 'tld',
+                      'shodan': 'shodan'}
+    result = {key: [] for key in search_strings}
+
+    parsed_data = ElementTree.parse(xml_file)
+    for key, value in search_strings.items():
+        occurences = parsed_data.findall(value)
+        for occurence in occurences:
+            finding = occurence.text
+            if finding:
+                result[key].append(finding)
+            else:
+                ip_adr = occurence.find('ip').text
+                host_name = occurence.find('hostname').text
+                finding = {host_name: ip_adr}
+                result[key].append(finding)
+    return result
+
+
+def save(json_file, result):
+    with open(json_file, 'w') as f:
+        json.dump(result, f)
 
 
 nc = NATS()
@@ -30,10 +79,6 @@ async def run(loop):
     except Exception as e:
         print('Error: %s' % e)
 
-    # start the dummy process
-    runtime = 60
-    now = 0
-
     try:
         status = api.JobStatus()
         status.id = analyzer_id
@@ -43,21 +88,7 @@ async def run(loop):
         await nc.publish(status_topic, status.SerializeToString())
         await nc.flush(0.500)
 
-        # define output paths
-        folder = '/data/' + domain
-        file_base = folder + '/' + scanner_id
-        html_file = file_base + '.html'
-        xml_file = file_base + '.xml'
-        json_file = file_base + '.json'
-        print('Saving to file {} and {}'.format(html_file, xml_file))
-
-        # define command
-        data_source = "bing"
-        _cmd = "cd {} && theharvester -d {} -b {} -f {}".format(folder, domain, data_source, html_file)
-
-        # run it
-        print("Executing: " + _cmd)
-        os.system(_cmd)
+        json_file, xml_file = execute(domain, scanner_id)
 
         status.completed = 50
         print('sending %s completed to nats topic: %s' % (status.completed, status_topic))
@@ -65,29 +96,15 @@ async def run(loop):
         await nc.flush(0.500)
 
         # parse the output
-        search_strings = {'email': 'email',
-                          'host_name': 'host',
-                          'virtual_host_name': 'vhost',
-                          'tld': 'tld',
-                          'shodan': 'shodan'}
-        result = {key: [] for key in search_strings}
+        result = parse(xml_file)
 
-        parsed_data = ElementTree.parse(xml_file)
-        for key, value in search_strings.items():
-            occurences = parsed_data.findall(value)
-            for occurence in occurences:
-                finding = occurence.text
-                if finding:
-                    result[key].append(finding)
-                else:
-                    ip_adr = occurence.find('ip').text
-                    host_name = occurence.find('hostname').text
-                    finding = {host_name: ip_adr}
-                    result[key].append(finding)
+        status.completed = 75
+        print('sending %s completed to nats topic: %s' % (status.completed, status_topic))
+        await nc.publish(status_topic, status.SerializeToString())
+        await nc.flush(0.500)
 
         # save as json
-        with open(json_file, 'w') as f:
-            json.dump(result, f)
+        save(json_file, result)
 
         status.completed = 100
         print('sending %s completed to nats topic: %s' % (status.completed, status_topic))
